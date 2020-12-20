bd35f0fd7493e0fe43e4295e97c8f87db64e18ec
Nitesh Kumar <nitesh04singh@gmail.com>
2020-12-20 09:49:13 +0800

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Dashboard from './Pages/dashboard'
+import Analytics from './Pages/analytics'
+import RandomList from './Pages/randomlist'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+
+class App extends Component {
+    
+    render() {
+        return (  
+            <Router>     
+                <Switch>                
+                <Route path="/analytics" component={Analytics} />
+                <Route path="/list" component={RandomList} />
+                <Route path="/"  exact component={Dashboard} />
+                </Switch>
+            </Router> 
+        )
+    }
+
 }
 
 export default App;
